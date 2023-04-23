a9f48f14e11554c2afba51552553d30e79157d03
Rong Wen <rteh0003@student.monash.edu>
2023-04-23 17:59:19 +0800

    testing 23/4

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ package src;
 
 public class main{
     public static void main(String[]args){
-        System.out.println("PLEASE WORK");
+        System.out.println("PLEASE WORK, TESINTG 2.o");
     }
 }
 
