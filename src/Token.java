559e90347d49366e6035bb9bf14ffc2c9e692b3b
KHOO KAI NENG <kkho0020@student.monash.edu>
2023-04-24 13:10:06 +0800

    added methods to Token class

--- src/Token.java
+++ src/Token.java
@@ -8,6 +8,7 @@ public class Token {
     private TokenState flying;
 
     private int id;
+    private ActionList allowableActions;
 
     public Token(int id) {
         this.id = id;
@@ -32,6 +33,24 @@ public class Token {
 
     public ActionList getAllowableActions() {
         return tokenState.getAllowableActions();
+    }
+
+    public void addAction(Action action) {
+        if(action == null){
+            throw new NullPointerException("Unable to add a null action!");
+        }
+        this.allowableActions.add(action);
+    }
+
+    public void removeAction(Action action){
+        if(action == null){
+            throw new NullPointerException("Unable to add a null action!");
+        }
+        this.allowableActions.remove(action);
+    }
+
+    public void clearActions(){
+        this.allowableActions = new ActionList();
     }
 
 
