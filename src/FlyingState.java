ba3d462b7c8695265a7f922883b8f1af3d2293fc
KHOO KAI NENG <kkho0020@student.monash.edu>
2023-04-25 21:30:26 +0800

    created setTokensAllowableActions functions

--- src/FlyingState.java
+++ src/FlyingState.java
@@ -1,18 +1,14 @@
 package src;
 
+import java.util.ArrayList;
+
 public class FlyingState implements PlayerState {
 
-    public Token token;
-    public Position moveToPosition;
-
-
-    public FlyingState(Token token, Position moveToPosition) {
-        this.token = token;
-        this.moveToPosition = moveToPosition;
+    public FlyingState() {
     }
 
+    public void setTokensAllowableActions(ArrayList<Token> tokens, ArrayList<Position> emptyPositions) {
 
+    }
 
-//    public ActionList getAllowableActions() {
-//    }
 }
