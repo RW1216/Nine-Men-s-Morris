fdf6e9aceddafc8e9bcadccfd9bc3a1b3cafdf77
Rong Wen <rteh0003@student.monash.edu>
2023-05-17 23:34:11 +0800

    Changed the number of token back to 9

--- src/Players/Player.java
+++ src/Players/Player.java
@@ -127,7 +127,7 @@ public abstract class Player {
      * Updates the player's state
      */
     public void updateSelfState() {
-        if (playerState == PlayerState.PLACING && tokensPlaced == 4) {
+        if (playerState == PlayerState.PLACING && tokensPlaced == 9) {
             playerState = PlayerState.MOVING;
         } else if (playerState == PlayerState.MOVING && tokenCount == 3) {
             playerState = PlayerState.FLYING;
